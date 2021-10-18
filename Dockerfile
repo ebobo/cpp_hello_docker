d3a4b2ae2bcf7875f3169aa5b6d0ff2f33fd1ef4
qi xu <qi.xu@autronicafire.no>
2021-10-18 11:48:43 +0200

    wip

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 FROM ubuntu:latest
 
+LABEL maintainer="qi.xu@carrier.com"
+
 # install some packages
 RUN ln -fs  /usr/share/zoneinfo/Europe/Oslo /etc/localtime
 RUN apt-get -y update && apt-get install -y --no-install-recommends
 RUN apt-get install clang cmake git libboost-all-dev -y
 
 WORKDIR /home/cpp/hello
-
 COPY . ./
+
 
 RUN cmake CMakeLists.txt
 RUN make
--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <thread>
 #include <chrono>
 #include <ctime>
+
 
 using namespace std;
 
@@ -32,16 +34,27 @@ int main()
     cout << "Hello Qi!" << endl;
     // current date/time based on current system
     time_t now = time(0);
-   
    // convert now to string form
     char* dt = ctime(&now);
-
+  
     cout << "The local date and time is: " << dt << endl;
+    this_thread::sleep_for(chrono::seconds(1));
+
+    cout << "I am writing some words down ... " << endl;
+    ofstream logfile;
+    logfile.open ("log.txt");
+    logfile << "----------------------------- \n" ;
+    logfile << "The local date and time is " + string(dt) + "\n" ;
+    logfile.close();
+
+    this_thread::sleep_for(chrono::seconds(1));
+    cout << "Test Docker Image Finish"<< endl;
+
    
-    thread worker_one(DoWork);
-    thread worker_two(DoRest);
-    worker_one.join();
-    worker_two.join();
+    // thread worker_one(DoWork);
+    // thread worker_two(DoRest);
+    // worker_one.join();
+    // worker_two.join();
     // cin.get();
     return 0;
 }
